707749afebd6b955f87aa50b7aa4bd3855aa215c
dionaosmani <120096662+dionaosmani@users.noreply.github.com>
2024-06-02 11:55:09 +0200

    updating app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,3 +18,41 @@ import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+
+function App() {
+    return (
+  
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route exact path='/' element={<HomeScreen/>}/> 
+              <Route exact path='/login' element={<LoginScreen/>}/>
+              <Route exact path='/register' element={<RegisterScreen/>}/>
+              <Route exact path='/shipping' element={<ShippingScreen/>}/>
+              <Route exact path='/payment' element={<PaymentScreen/>}/>
+              <Route exact path='/placeorder' element={<PlaceOrderScreen/>}/>
+              <Route exact path='/order/:id' element={<OrderScreen/>}/>
+              <Route exact path='/profile' element={<ProfileScreen/>}/>
+              <Route exact path='/product/:id' element={<ProductScreen/>}/> 
+              <Route exact path='/cart/:id' element={<CartScreen/>}/> 
+              <Route exact path='/cart' element={<CartScreen/>}/> 
+  
+              <Route exact path='/admin/userlist' element={<UserListScreen/>}/>
+              <Route exact path='/admin/user/:id/edit' element={<UserEditScreen/>}/>
+  
+              <Route exact path='/admin/productlist' element={<ProductListScreen/>}/>
+              <Route exact path='/admin/product/:id/edit' element={<ProductEditScreen/>}/>
+  
+              <Route exact path='/admin/orderlist' element={<OrderListScreen/>}/>
+  
+            </Routes>
+          </Container>
+        </main>
+        <Footer /> 
+      </Router>
+    );
+  }
+  
+  export default App;
